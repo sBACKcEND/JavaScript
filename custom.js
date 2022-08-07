8ca2039f62df77faf29fe790f4c654bc7f284962
Šarūnas <107551364+sBACKcEND@users.noreply.github.com>
2022-08-07 04:30:16 +0300

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,117 +1,91 @@
-function hamburger() {
-  let x = document.getElementById("links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+// Hamburger Menu  $$$$$$$$$$$$$$$$$$$$
+function menuOnClick() {
+  document.getElementById("menuH-barH").classList.toggle("changeH");
+  document.getElementById("navH").classList.toggle("changeH");
+  document.getElementById("menuH-bgH").classList.toggle("changeH-bgH");
 }
 
+// Control Panel section  $$$$$$$$$$$$$$$$$$$$
+let conDiv = document.querySelector(".control_div");
 function openCloseDiv() {
-  let x = document.getElementById("controlDiv");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  if (conDiv.style.display === "block") {
+    conDiv.style.display = "none";
   } else {
-    x.style.display = "block";
+    conDiv.style.display = "block";
   }
 }
 
 function changeHeight() {
-  let x = document.getElementById("controlDiv");
-  x.style.height = "150px";
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+  conDiv.classList.toggle("control_div1");
 }
 
 function changeWidth() {
-  let x = document.getElementById("controlDiv");
-  x.style.width = "150px";
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+  conDiv.classList.toggle("control_div2");
 }
 
 function moveToCenter() {
-  let x = document.getElementById("controlDiv");
-  x.style.margin = "0 auto";
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+  conDiv.classList.toggle("control_div3");
 }
 
 function changePosition() {
-  let x = document.getElementById("controlDiv");
-  x.style.position = "absolute";
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+  conDiv.classList.toggle("control_div4");
+}
+
+// Color section  $$$$$$$$$$$$$$$$$$$$
+function rand() {
+  return Math.floor(Math.random() * 256);
+}
+
+function randomColor() {
+  return "rgb(" + rand() + "," + rand() + "," + rand() + ")";
+}
+
+let squares = document.querySelectorAll(".color_square");
+
+function recolor() {
+  for (i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = randomColor();
   }
 }
 
-// function recolor(event) {
-//   event.target.style.backgroundColor = randomColor();
-// }
-// function randomColor() {
-//   return "rgb(" + rand() + "," + rand() + "," + rand() + ")";
-// }
-// function rand() {
-//   return Math.floor(Math.random() * 256);
-// }
+// function addColorName() {
+//     for (i = 0; i < squares.length; i++) {
+//       squares[i].innerHTML = randomColor();
+//     }
+//   }
 
-let colors = [
-    "#FFE4C4",
-    "#000000",
-    "#FFEBCD",
-    "#0000FF",
-    "#8A2BE2",
-    "#A52A2A",
-    "#DEB887",
-    "#5F9EA0",
-    "#7FFF00",
-    "#D2691E",
-    "#6495ED",
-    "#FFF8DC",
-    "#DC143C",
-    "#00FFFF",
-    "#00008B",
-    "#008B8B",
-    "#B8860B",
-    "#A9A9A9",  
-];
-let boxes = document.querySelectorAll(".square");
-function recolor(event) {
-  for (i = 0; i < boxes.length; i++) {
-    boxes[i].style.backgroundColor =
-      colors[Math.floor(Math.random() * colors.length)];
-  }
+// Data section  $$$$$$$$$$$$$$$$$$$$
+function addTable() {
+  let tbody = document.getElementById("tbody");
+  let tbodyRow = tbody.insertRow();
+  let cell1 = tbodyRow.insertCell(0);
+  let cell2 = tbodyRow.insertCell(1);
+  let cell3 = tbodyRow.insertCell(2);
+  let cell4 = tbodyRow.insertCell(3);
+
+  cell1.innerHTML = addRowNum();
+
+  let firstName = document.querySelector("#firstName").value;
+  cell2.innerText = firstName;
+
+  let lastName = document.querySelector("#lastName").value;
+  cell3.innerText = lastName;
+
+  let ageCount = document.querySelector("#countAge").value;
+  let output = new Date().getFullYear() - Number.parseInt(ageCount);
+  cell4.innerText = output;
 }
 
-function display() {
-  document.getElementById("displayarea").innerHTML = +1;
-  document.getElementById("displayarea1").innerHTML =
-    document.getElementById("fname").value;
-  document.getElementById("fname").value = "";
-  document.getElementById("displayarea2").innerHTML =
-    document.getElementById("lname").value;
-  document.getElementById("lname").value = "";
-  document.getElementById("displayarea3").innerHTML =
-    document.getElementById("year").value;
-  document.getElementById("year").value = "";
+let j = 0;
+function addRowNum() {
+  addTable.value = j++;
+  return j;
 }
 
-// function resetTable() {
-//     document.getElementById("newTable").reset(td);
-//  }
-
- function resetTable() {
-	let Table = document.getElementById("newTable");
-	Table.innerHTML = "";
+function resetRowNum() {
+  j === 0;
 }
+
+$("#resetButton").click(function () {
+  $("#tbody").empty();
+});
